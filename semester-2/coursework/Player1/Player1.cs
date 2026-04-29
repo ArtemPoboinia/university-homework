a6b998f90b27c50b415e5559d0f20f06acde3c0a
Artem Poboinia <artempoboinia@gmail.com>
2026-04-29 17:08:49 +0300

    Начальная структура курсовой

---
+++ semester-2/coursework/Player1/Player1.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CardFool
+{
+    public class MPlayer1
+    {
+        private string Name = "Prototype 0.1";
+        private List<SCard> hand = new List<SCard>();       // карты на руке
+        private Suits trumpSuit; // Козырная масть
+
+        // Возвращает имя игрока
+        public string GetName()
+        {
+            return Name;
+        }
+        //Возвращает количество карт на руке
+        public int GetCount()
+        {
+            return hand.Count;
+        }
+        //Добавление карты в руку, во время добора из колоды, или взятия карт
+        public void AddToHand(SCard card)
+        {
+            hand.Add(card);
+            SortHand();
+        }
+
+        private void SortHand()
+        {
+            hand = hand.OrderBy(c => c.Suit == trumpSuit) // False (0) идет перед True (1)
+                       .ThenBy(c => c.Rank)
+                       .ToList();
+        }
+
+        //Начальная атака
+        public List<SCard> LayCards()
+        {
+            return [];
+        }
+
+        //Защита от карт
+        //На вход подается набор карт на столе, часть из них могут быть уже покрыты
+        public bool Defend(List<SCardPair> table)
+        {
+            return false;
+        }
+        //Добавление карт
+        //На вход подается набор карт на столе, а также отбился ли оппонент
+        public bool AddCards(List<SCardPair> table, bool OpponentDefenced)
+        {
+            return false;
+        }
+        //Вызывается после основной битвы, когда известно отбился ли защищавшийся
+        //На вход подается набор карт на столе, а также была ли успешной защита
+        public void OnEndRound(List<SCardPair> table, bool IsDefenceSuccesful)
+        {
+
+        }
+        //Установка козыря, на вход подаётся козырь, вызывается перед первой раздачей карт
+        public void SetTrump(SCard NewTrump)
+        {
+            trumpSuit = NewTrump.Suit;
+        }
+    }
+}
